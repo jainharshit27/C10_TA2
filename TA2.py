822b015bc3319655639637db318dd68c98026d6f
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-11-02 12:50:32 +0530

    Update TA2.py

--- TA2.py
+++ TA2.py
@@ -9,8 +9,16 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 #pymunk space
+#Add gravity and assign 1000.               # for SA make this 0, for AA2 make it 1000
+#Add wind and assign 0.                     # for SA change this to value between 100-1000, for AA2 keep the value of SA
 space = pymunk.Space()
+#Declare space.gravity and assign wind, gravity to it.
 draw_options = pymunk.pygame_util.DrawOptions(screen)
+
+#Add body and assign pymunk.Body(1,100).
+#Add shape and assign pymunk.Circle(body, 25).
+#Add body.position and assign 200, 100
+#Add body and shape to space using space.add(body, shape).
 
 while True:
     screen.fill((255,255,255))
@@ -19,6 +27,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    #Add space.debug_draw(draw_options) to draw the created body.
     pygame.display.update()
     
     #space reload
